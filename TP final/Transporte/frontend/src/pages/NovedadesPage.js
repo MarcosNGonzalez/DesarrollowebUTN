e8a9f62c5e6879d4d1b28226bca7daab1569afff
Marcos Gonzalez <gnzlz.mrcs@gmail.com>
2022-08-25 15:01:47 -0300

    asd

--- TP final/Transporte/frontend/src/pages/NovedadesPage.js
+++ TP final/Transporte/frontend/src/pages/NovedadesPage.js
@@ -3,13 +3,6 @@ import axios from 'axios';
 import NovedadItem from '../components/novedades/NovedadItem';
 
 
-
-
-
-
-
-
-
 const NovedadesPage = (props) => {
     const [loading, setLoading] = useState(false);
     const [novedades, setNovedades] = useState([]);
@@ -18,7 +11,7 @@ const NovedadesPage = (props) => {
     useEffect(() => { 
         const cargarNovedades = async () => { 
             setLoading(true);
-            const response = await axios.get('http://localhots:3000/api/novedades');
+            const response = await axios.get('http://localhost:3000/api/novedades');
             setNovedades(response.data);
             setLoading(false);
         };
